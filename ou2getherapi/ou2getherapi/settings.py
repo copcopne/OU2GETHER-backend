aadd15e77c4d49f57661f9030a3d57a565320dee
Si Nguyen <130967183+copcopne@users.noreply.github.com>
2025-03-29 21:57:32 +0700

    Add STATIC_ROOT configuration for deployment

--- ou2getherapi/ou2getherapi/settings.py
+++ ou2getherapi/ou2getherapi/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/home/copcopne/staticfiles" # Cấu hình pythonanywhere
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
