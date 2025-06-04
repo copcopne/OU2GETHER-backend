cbcfd38feb3d931051f5d571bd4f5db900447acc
Si Nguyen <130967183+copcopne@users.noreply.github.com>
2025-06-04 16:39:46 +0700

    feat: add stats reporting functionality to admin site with dynamic filters

--- ou2getherapi/ou2gether/admin.py
+++ ou2getherapi/ou2gether/admin.py
@@ -2,14 +2,92 @@ from django.contrib import admin
 from django.template.response import TemplateResponse
 from django.urls import path
 
-from ou2gether.models import User, Post, Comment, Group
-from django.utils.html import mark_safe
-from django import forms
-from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from ou2gether import models
+from django.db.models import Count
+from django.db.models.functions import TruncDate, TruncMonth
+from datetime import datetime
+from django.utils import timezone
+from django.utils.timezone import make_aware
 
 
 class ou2getherAdminSite(admin.AdminSite):
     site_header = "Hệ thống mạng xã hội cựu sinh viên OU (OU2gether)"
+
+    def get_urls(self):
+        return [path('stats/', self.stats, name='stats')] + super().get_urls()
+
+    def stats(self, request):
+        object_type = request.GET.get('object_type', '')
+        report_type = request.GET.get('report_type', '')
+        month_str = request.GET.get('month', '')
+        quarter_str = request.GET.get('quarter', '')
+        year_str = request.GET.get('year', timezone.now().year)
+
+        qs = models.User.objects.filter(is_active=True, is_verified=True) if object_type == 'users' else models.Post.objects.filter(is_active=True)
+
+        year = timezone.now().year
+        quarter = 0
+        month = 0
+        try:
+            year = int(year_str) if year_str else year
+
+            quarter = int(quarter_str) if quarter_str else 0
+            if quarter < 0 or quarter > 4:
+                quarter = 0
+
+            month = int(month_str) if month_str else 0
+            if month < 1 or month > 12:
+                month = 0
+
+        except ValueError:
+            year = timezone.now().year
+            quarter = 0
+            month = 0
+
+        label = ''
+        count = 0
+        if report_type == 'quarter':
+            if quarter > 0:
+                start_month = (quarter - 1) * 3 + 1
+                end_month = start_month + 2
+                start = make_aware(datetime(year, start_month, 1))
+                if end_month == 12:
+                    end = make_aware(datetime(year + 1, 1, 1))
+                else:
+                    end = make_aware(datetime(year, end_month + 1, 1))
+
+                count = qs.filter(created_at__gte=start, 
+                                created_at__lt=end) \
+                                .count()
+                label = f"Q{quarter}"
+
+        elif report_type == 'month':
+            if month > 0:
+                count = qs.filter(created_at__year=year, created_at__month=month).count()
+                label = f"Tháng {month}"
+        
+        elif report_type == 'year':
+                count = qs.filter(created_at__year=year).count()
+                label = f"Năm {year}"
+
+        data = {
+            'label': label,
+            'count': count,
+            'total': qs.count() if label else 0
+        }
+        
+        context = {
+            'object_type': object_type,
+            'report_type': report_type,
+            'month_value': month_str,
+            'quarter_value': quarter_str,
+            'year_value': year_str,
+            'year_list': [y for y in range(datetime.now().year, datetime.now().year - 6, -1)],
+            'data': data,
+        }
+
+        return TemplateResponse(request, 'admin/stats.html', context)
+
 
 admin_site = ou2getherAdminSite(name="myadmin")
 
@@ -72,7 +150,7 @@ class GroupAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_per_page = 10
 
-admin_site.register(User, UserAdmin)
-admin_site.register(Post, PostAdmin)
-admin_site.register(Group, GroupAdmin)
-admin_site.register(Comment, CommentAdmin)
+admin_site.register(models.User, UserAdmin)
+admin_site.register(models.Post, PostAdmin)
+admin_site.register(models.Group, GroupAdmin)
+admin_site.register(models.Comment, CommentAdmin)
